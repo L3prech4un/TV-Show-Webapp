ca26cf097b1656af80b5fce2309c49f7b058b0a5
Gregory Tvedt <gregtv05@gmail.com>
2025-10-29 23:46:10 -0400

    I added some queries

--- db/query.py
+++ db/query.py
@@ -1,5 +1,6 @@
 """query.py: Uses SQLAlchemy to create generic queries for interacting with the Postgres database"""
 from db.server import get_session       # import get_session function from server.py
+from sqlalchemy import text 
 
 def get_all(table) -> list:
     """Select all records from a table using SQLAlchemy
@@ -35,4 +36,85 @@ def insert(record) -> None:
         print(f"Error inserting record: {e}")
     finally:
         # Closes the session
+        session.close()
+
+def changePost(userid: int, postid: int, newContent: str) -> bool:
+    "Allow User to change the text of what they post"
+    session = get_session()
+    try:
+        # Tries to update the post 
+        query = """
+        UPDATE POST
+        SET CONTENT = :NEWCONTENT
+        WHERE POSTID = :POSTID
+        AND POSTID IN (
+            SELECT C.POSTID
+            FROM CREATES C
+            WHERE C.USERID = :USERID);
+        """
+        session.execute(text(query), {"USERID": userid, "POSTID": postid, "NEWCONTENT": newContent })
+        session.commit()
+        return True
+    except Exception as e:
+        session.rollback()
+        print("Error Updating post:", e)
+        return False
+    finally:
+        session.close()
+    
+def deletePost(userid: int, postid: int) -> bool:
+    "Allow User to delete a post"
+    session = get_session()
+    try:
+        # Tries to delete the post
+        session.execute("""DELETE COMMENT WHERE POSTID = :POSTID""", {"POSTID": postid})
+       
+        session.execute(
+        """
+            DELETE POST WHERE POSTID = :POSTID
+            AND POSTID IN (
+            SELECT C.POSTID
+            FROM CREATES C
+            WHERE C.USERID = :USERID);
+        """,
+        {"POSTID": postid, "USERID": userid})
+
+        session.execute("""DELETE CREATES WHERE POSTID = :POSTID;""", {"POSTID": postid})
+
+        session.commit()
+        return True
+    except Exception as e:
+        session.rollback()
+        print("Error Deleting Post:", e)
+        return False
+
+def getFeed(userid: int) -> list:
+    "Create the Users feed from the people they follow"
+    session = get_session()
+    try:
+        # Tries to make a User's feed from follower posts
+        query = """
+        SELECT P.TITLE, P.DATE, P.CONTENT 
+        FROM USER U1, USER U2, FOLLOWS F, CREATES C, POST P
+        WHERE U1.USERID = F.USERID
+        AND F.FOLLOWERID = U2.USERID
+        AND U2.USERID = C.USERID
+        AND C.POSTID = P.POSTID
+        AND U1.USERID = :USER_ID
+
+        UNION
+
+        SELECT P.TITLE, P.DATE, P.CONTENT
+        FROM CREATES C, POST P
+        WHERE C.USERID = :USER_ID
+        AND C.POSTID = P.POSTID
+        
+        ORDER BY DATE DESC;
+        """
+        result = session.execute(text(query), {"USER_ID": userid}).fetchall()
+        return result
+    except Exception as e:
+        session.rollback()
+        print("Error loading feed:", e)
+    finally:
         session.close()
